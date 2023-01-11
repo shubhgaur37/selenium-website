bf34b23ae915e0e1f3f4416119dd9ed20390b613
shubhgaur37 <42857733+shubhgaur37@users.noreply.github.com>
2023-01-11 12:14:40 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo docker build ./mysql/ -t db
-sudo docker run -it -d -p 3306:3306 --name mysql db
+sudo docker run --platform linux/amd64 -it -d -p 3306:3306 --name mysql db
 
 sudo docker build ./website/ -t web
-sudo docker run -it -d -p 3001:80 --link mysql:db web
+sudo docker run --platform linux/amd64 -it -d -p 3001:80 --link mysql:db web
